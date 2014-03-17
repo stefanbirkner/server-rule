5dfa43ab07e8a0044da215590a76ada56fbff05c
Stefan Birkner <mail@stefan-birkner.de>
2014-03-17 09:00:56 +0100

    Reformat code according to IntelliJ's code style.

--- src/main/java/com/github/stefanbirkner/serverrule/HttpServer.java
+++ src/main/java/com/github/stefanbirkner/serverrule/HttpServer.java
@@ -7,48 +7,48 @@ import org.junit.rules.ExternalResource;
 /**
  * The {@code HttpServer} rule starts and stops a HttpServer server at port 8080. The
  * webapp's directory is {@code src/test/webapp}.
- * 
- * <p>
+ * <p/>
+ * <p/>
  * The test below checks the page source of a page provided by HttpServer.
- * 
+ * <p/>
  * <pre>
  * public class HttpServerTest {
  * 	&#064;Rule
  * 	public final HttpServer server = new HttpServer();
- * 
+ *
  * 	&#064;Test
  * 	public void povidesIndexJsp() throws Exception {
  * 		URL url = new URL(&quot;http://localhost:8080/&quot;);
  * 		InputStream is = url.openStream();
  * 		String pageSource = IOUtils.toString(is);
  * 		assertEquals(&quot;test successful&quot;, pageSource);
- * 	}
+ *    }
  * }
  * </pre>
  */
 public class HttpServer extends ExternalResource {
-	private Server server;
+    private Server server;
 
-	@Override
-	protected void before() throws Throwable {
-		server = new Server(8080);
-		WebAppContext webapp = new WebAppContext();
-		webapp.setContextPath("/");
-		webapp.setResourceBase("src/test/webapp");
-		webapp.setParentLoaderPriority(true);
+    @Override
+    protected void before() throws Throwable {
+        server = new Server(8080);
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setResourceBase("src/test/webapp");
+        webapp.setParentLoaderPriority(true);
 
-		server.setHandler(webapp);
-		server.start();
-	}
+        server.setHandler(webapp);
+        server.start();
+    }
 
-	@Override
-	protected void after() {
-		try {
-			server.stop();
-		} catch (RuntimeException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Override
+    protected void after() {
+        try {
+            server.stop();
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/github/stefanbirkner/serverrule/HttpServerTest.java
+++ src/test/java/com/github/stefanbirkner/serverrule/HttpServerTest.java
@@ -1,23 +1,23 @@
 package com.github.stefanbirkner.serverrule;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.InputStream;
-import java.net.URL;
-
 import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 
-public class HttpServerTest {
-	@Rule
-	public final HttpServer server = new HttpServer();
+import java.io.InputStream;
+import java.net.URL;
 
-	@Test
-	public void povidesIndexJsp() throws Exception {
-		URL url = new URL("http://localhost:8080/");
-		InputStream is = url.openStream();
-		String pageSource = IOUtils.toString(is);
-		assertEquals("test successful", pageSource);
-	}
+import static org.junit.Assert.assertEquals;
+
+public class HttpServerTest {
+    @Rule
+    public final HttpServer server = new HttpServer();
+
+    @Test
+    public void povidesIndexJsp() throws Exception {
+        URL url = new URL("http://localhost:8080/");
+        InputStream is = url.openStream();
+        String pageSource = IOUtils.toString(is);
+        assertEquals("test successful", pageSource);
+    }
 }
